4fbdd6cd4070e05a961e8e1d6b8c05e8218bd078
Vinícius Ramaciotti <ramaciotti86@gmail.com>
2020-10-21 12:23:52 +0100

    Adding import

--- src/test/java/com/gov/location/util/HaversineImplTest.java
+++ src/test/java/com/gov/location/util/HaversineImplTest.java
@@ -1,6 +1,7 @@
 package com.gov.location.util;
 
 import com.gov.location.model.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
